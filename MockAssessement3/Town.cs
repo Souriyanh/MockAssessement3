488b2682bcb6d07033eadb1ee1cfd4d0452bfe02
AzureAD\SVongsiprasom <souriyanh@peopleit.com>
2022-11-11 17:44:25 -0500

    mock3draft

---
+++ MockAssessement3/Town.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssessement3
+{
+    public class Town
+    {
+        public Town(List<Villager> villagers)
+        {
+            Villagers = villagers;
+        }
+
+        public List<Villager> Villagers { get; set; }
+
+        //public void AddVillagers()
+        //{
+        //    var villager1 = new Villager;
+        //    var villager2 = new Villager;
+        //    var villager3 = new Villager;
+        //    var villager4 = new Villager;
+        //    villager4.Name=""
+        //}
+
+    }
+
+
+}
---
+++ MockAssessement3/Villager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssessement3
+{
+    public abstract class Villager
+    {
+        public abstract string Name { get; set; }    
+        public int Hunger { get; set; }
+
+         abstract public int Farm();
+      
+    }
+
+    public class Farmer : Villager
+    {
+         int Hunger = 1;
+        public override int Farm()
+        {
+           return Hunger;
+        }
+    }
+
+    public class Slacker : Villager
+    {
+        public int Hunger = 3;
+        public override int Farm()
+        {
+            return Hunger;
+        }
+    }
+
+
+}
